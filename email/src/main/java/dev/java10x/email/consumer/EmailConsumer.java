56fc0b086253246782d0a48d7f51cb1cc08350be
enzosdev <enzohfs@gmail.com>
2026-04-23 18:35:19 -0300

    feat: add listener method to listen the rabbitmq queue

--- email/src/main/java/dev/java10x/email/consumer/EmailConsumer.java
+++ email/src/main/java/dev/java10x/email/consumer/EmailConsumer.java
@@ -1,4 +1,20 @@
 package dev.java10x.email.consumer;
 
+
+import dev.java10x.email.dto.EmailDTO;
+import org.hibernate.annotations.Comment;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.stereotype.Component;
+
+@Component
 public class EmailConsumer {
+
+    @RabbitListener(queues = "email-queue") // essa annotation pega a fila e fica escutando
+    public void ListenEmailQueue(@Payload String string) {
+        //@payload quer dizer que esse parametro do metodo vai receber o conteudo principal da mensagem
+        //converte automaticamente de json para java
+        System.out.println("Received: " + string);
+
+    }
 }

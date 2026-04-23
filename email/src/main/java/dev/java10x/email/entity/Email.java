765d5927ad3b60c65ed865c520907c8c3ec6d929
enzosdev <enzohfs@gmail.com>
2026-04-23 18:30:45 -0300

    feat: add email entity

--- email/src/main/java/dev/java10x/email/entity/Email.java
+++ email/src/main/java/dev/java10x/email/entity/Email.java
@@ -1,4 +1,33 @@
 package dev.java10x.email.entity;
 
+import dev.java10x.email.enums.EmailStatus;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(name = "emails")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Email {
+
+    private final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID emailId;
+    private UUID userId;
+    private String emailFrom;
+    private String emailTo;
+    private String emailSubject;
+    @Column(columnDefinition = "BODY")
+    private String body;
+    private LocalDateTime sendDateEmail;
+    private EmailStatus emailStatus;
+
 }
+

6b02b06519075c8fb2490017f24ccce5559fe771
enzosdev <enzohfs@gmail.com>
2026-04-24 22:13:48 -0300

    feat: add create user endpoint

--- user/src/main/java/dev/java10x/user/controller/UserController.java
+++ user/src/main/java/dev/java10x/user/controller/UserController.java
@@ -1,4 +1,27 @@
 package dev.java10x.user.controller;
 
+import dev.java10x.user.dto.UserRequest;
+import dev.java10x.user.dto.UserResponse;
+import dev.java10x.user.service.UserService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping( "/users")
 public class UserController {
+    private UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping()
+    public ResponseEntity<UserResponse> createUser(@RequestBody UserRequest userRequest) {
+        UserResponse savedUser = userService.createUser(userRequest);
+        return ResponseEntity.status(HttpStatus.CREATED).body(savedUser);
+    }
 }

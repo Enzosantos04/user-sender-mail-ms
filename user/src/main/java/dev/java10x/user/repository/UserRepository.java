686bb63385739d4ffe59d668e57186a5a8de2c9f
enzosdev <enzohfs@gmail.com>
2026-04-24 22:13:06 -0300

    feat: add user repository using spring data jpa

--- user/src/main/java/dev/java10x/user/repository/UserRepository.java
+++ user/src/main/java/dev/java10x/user/repository/UserRepository.java
@@ -1,4 +1,10 @@
 package dev.java10x.user.repository;
 
-public interface UserRepository {
+import dev.java10x.user.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+@Repository
+public interface UserRepository extends JpaRepository<User, UUID> {
 }

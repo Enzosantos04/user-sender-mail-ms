274972631f84804d37b7e738b1b6071d142e1013
enzosdev <enzohfs@gmail.com>
2026-04-24 22:14:52 -0300

    feat: add user request as dto

--- user/src/main/java/dev/java10x/user/dto/UserRequest.java
+++ user/src/main/java/dev/java10x/user/dto/UserRequest.java
@@ -1,4 +1,18 @@
 package dev.java10x.user.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 public class UserRequest {
+    @NotBlank(message = "name is required")
+    private String name;
+    @NotBlank(message = "name is required")
+    @Email(message = "email is wrong format")
+    private String email;
 }

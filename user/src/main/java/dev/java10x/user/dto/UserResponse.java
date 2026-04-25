7448e847210463d5f10aeca8b1b553ec72883d0d
enzosdev <enzohfs@gmail.com>
2026-04-24 22:16:23 -0300

    feat: add user response as dto

--- user/src/main/java/dev/java10x/user/dto/UserResponse.java
+++ user/src/main/java/dev/java10x/user/dto/UserResponse.java
@@ -1,4 +1,15 @@
 package dev.java10x.user.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 public class UserResponse {
+    private String name;
+    private String email;
 }

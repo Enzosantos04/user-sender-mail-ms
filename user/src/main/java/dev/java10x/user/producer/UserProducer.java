6df0abe8488d1dcc87cd8feaa59934520291fc38
enzosdev <enzohfs@gmail.com>
2026-04-24 22:18:07 -0300

    feat: set up user producer to send data to rabbitmq queue

--- user/src/main/java/dev/java10x/user/producer/UserProducer.java
+++ user/src/main/java/dev/java10x/user/producer/UserProducer.java
@@ -1,4 +1,22 @@
 package dev.java10x.user.producer;
 
+import dev.java10x.user.dto.UserRequest;
+import dev.java10x.user.dto.UserResponse;
+import dev.java10x.user.entity.User;
+import dev.java10x.user.repository.UserRepository;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.stereotype.Component;
+
+@Component
 public class UserProducer {
+    private final RabbitTemplate rabbitTemplate;
+
+    public UserProducer(RabbitTemplate rabbitTemplate) {
+        this.rabbitTemplate = rabbitTemplate;
+    }
+
+    public void send(@Payload UserRequest user) {
+        rabbitTemplate.convertAndSend(user);
+    }
 }

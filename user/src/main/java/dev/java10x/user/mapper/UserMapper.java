e84fc676e821e7ca4abaf0b05d34e0bf83004903
enzosdev <enzohfs@gmail.com>
2026-04-24 22:17:25 -0300

    feat: convert dto to entity and entity to response dto

--- user/src/main/java/dev/java10x/user/mapper/UserMapper.java
+++ user/src/main/java/dev/java10x/user/mapper/UserMapper.java
@@ -1,4 +1,30 @@
 package dev.java10x.user.mapper;
 
+
+import dev.java10x.user.dto.UserRequest;
+import dev.java10x.user.dto.UserResponse;
+import dev.java10x.user.entity.User;
+import org.springframework.stereotype.Component;
+
+@Component
 public class UserMapper {
+
+
+    public User toEntity(UserRequest userRequest) {
+        User newUser = new User();
+        newUser.setName(userRequest.getName());
+        newUser.setEmail(userRequest.getEmail());
+        return newUser;
+    }
+
+
+    public UserResponse toResponse(User user) {
+        if (user == null) {
+            return null;
+        }
+        UserResponse newUserResponse = new UserResponse();
+        newUserResponse.setName(user.getName());
+        newUserResponse.setEmail(user.getEmail());
+        return newUserResponse;
+    }
 }

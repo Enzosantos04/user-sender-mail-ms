86a46baa7696cccb181ca49816c27514c73aac62
enzosdev <enzohfs@gmail.com>
2026-04-24 22:14:17 -0300

    feat: add user entity

--- user/src/main/java/dev/java10x/user/entity/User.java
+++ user/src/main/java/dev/java10x/user/entity/User.java
@@ -1,4 +1,25 @@
 package dev.java10x.user.entity;
 
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "users")
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 public class User {
+
+    private final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID userId;
+    private String name;
+    private String email;
+
 }
